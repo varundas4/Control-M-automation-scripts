ec61878e9adf6c1cdbf5ffcba9fa05cc6ad1edc8
Varun Das <varundas4@gmail.com>
2019-03-18 15:57:04 +0530

    Update core_check.sh
    Script to check for availability of an undesirable CORE DUMP file in Control-M EM/Server's path

--- core_check.sh
+++ core_check.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+#################################################################################################################################
+##Script Name: core_check.sh                                                                                                   ##
+##Description: Script to check for availability of an CORE DUMP file in Control-M EM/Server's path. The file is undesirable and##
+##             may cause problems in proper functionality of Control-M services.                                               ##
+##Created by: Varun Das                                                                                                        ##
+##Date: 5th February 2019                                                                                                      ##
+#################################################################################################################################
 
-set -x;
-cd $HOME
-find -L *[Cc][Oo][Rr][Ee]*
+set +x;
+cd $HOME #path to check for 'core dump' file
+find -L *[Cc][Oo][Rr][Ee]* #case-insensitive search
 RESULT=$?
 echo "result: $RESULT"
 if [ $RESULT -eq 0 ]; then 
@@ -14,6 +21,3 @@ status=0
 echo "status= $status"
 exit $status
 fi
-
-
-
