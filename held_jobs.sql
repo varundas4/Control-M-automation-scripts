263c26e04593058582193f2cda0177bfc204681f
Varun Das <varundas4@gmail.com>
2019-04-21 19:23:47 +0530

    Update held_jobs.sql

--- held_jobs.sql
+++ held_jobs.sql
@@ -1,3 +1,11 @@
+/*##############################################
+##Script name: held_jobs.sql	              ##
+##Description: Lists out active Control-M jobs##
+##	       that have been put on hold     ##	      
+##Created & Tested by: Varun Das 	      ##			      
+##Date: 19th November 2018                    ##
+##############################################*/					
+
 SELECT * FROM CMR_AJF;
 
 SELECT jobname, orderid, status 
